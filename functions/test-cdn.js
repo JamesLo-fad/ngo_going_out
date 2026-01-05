665d81b782b72c51dc482418884ceadb8136cf6a
james lo <james.lo@antalpha.com>
2026-01-06 01:39:12 +0800

    Remove test endpoint - CDN is now working

--- functions/test-cdn.js
+++
@@ -1,7 +0,0 @@
-// Test endpoint to verify Pages Functions routing
-export async function onRequest(context) {
-  return new Response('Test endpoint works!', {
-    status: 200,
-    headers: { 'Content-Type': 'text/plain' }
-  });
-}
